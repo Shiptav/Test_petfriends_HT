eef556e6aa076e6c8bb5ed80d123b2d41697bec9
unknown <shiptav@mail.ru>
2023-03-16 00:20:28 +0300

    Home task 24.7.2

--- Tests/api.py
+++ Tests/api.py
@@ -129,8 +129,8 @@ class PetFriends:
         return status, result
     
     def add_new_pet_photo(self, auth_key: json, pet_id: str, pet_photo: str) -> json:
-        """Метод отправляет (постит) на сервер данные о добавляемом питомце и возвращает статус
-        запроса на сервер и результат в формате JSON с данными добавленного питомца"""
+        """Метод отправляет (постит) на сервер фото питомца и возвращает статус
+        запроса на сервер"""
 
         data = MultipartEncoder(
             fields={
